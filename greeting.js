4a109a7fa316906de42bf8aa897829c40ba091de
Austin Smith <austinleesmith97@gmail.com>
2019-09-18 19:51:04 -0700

    greeting js

---
+++ greeting.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var hourNow = today.getHours;
+var greeting;
+if (hourNow > 18) {
+    greeting = 'We hope you are having a good night! ';
+} else if (hourNow > 12) {
+    greeting = 'Perfect time to look at some dogs! ';
+} else if (hourNow > 0) {
+    greeting = 'Early to the game! ';
+} else {
+    greeting = 'Welcome!';
+
+    document.write('<h2>' + greeting + '</h2>');
+}
